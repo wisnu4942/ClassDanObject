688939f6c1456b9447238d2208a9191b5d5f2c8a
wisnu4942 <127730366+wisnu4942@users.noreply.github.com>
2023-03-20 18:14:07 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
-﻿namespace LatihanClassDanObject
+﻿namespace LatihanClassDanObject2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Mahasiswa saya = new Mahasiswa();
-            Mahasiswa kamu = new Mahasiswa();
+            Taxi taxi = new Taxi();
 
-            saya.Nama = "Wisnu";
-            saya.Nim = "22.11.4942";
-            saya.Ipk = 3.90f;
+            taxi.DriverName = "Rojali";
+            taxi.OnDuty = false;
+            taxi.NumPassanger = 5;
 
-            kamu.Nama = "Rosa";
-            kamu.Nim = "22.11.2494";
-            kamu.Ipk = 2.90f;
-
-            saya.Data();
-            saya.Registrasi();
-            saya.IsiKrs();
-
-            kamu.Data();
-            kamu.Registrasi();
-            kamu.IsiKrs();
+            taxi.TaxiInfo();
+            taxi.PicUpPassanger();
+            taxi.DropOffPassanger();
 
             Console.ReadKey();
-            Console.WriteLine("Hello, World!");
         }
     }
 }
---
+++ Taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject2
+{
+    public class Taxi
+    {
+        public string DriverName { get;set; }
+        public bool OnDuty { get;set; }
+        public int NumPassanger { get;set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty: Yes");
+            } else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number of Passanger: {0}", NumPassanger);
+        }
+        public void PicUpPassanger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
